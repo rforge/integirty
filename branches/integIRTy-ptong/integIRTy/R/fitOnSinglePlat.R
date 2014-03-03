641267d00a1d11f2d9358c106beddef148d4a773
nickytong <nickytong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-03 20:04:39 +0000

    reparented project guts into branch
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/integirty@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/integIRTy-ptong/integIRTy/R/fitOnSinglePlat.R
@@ -0,0 +1,41 @@
+fitOnSinglePlat <-
+function(data, model=2, guessing=FALSE, 
+							sampleIndices=1:ncol(data), 
+							geneIndices=1:nrow(data), ...)
+{
+	## tpm fits a guessing parameter in 2 modes: one is guessing in unconstrained rasch,
+	## the other is guessing in 2PL.
+	## if guessing=T, then max.guessing=1 is used to fit the guessing parameter for every item
+	#K <- length(sampleIndices) # item size
+	if(is.null(sampleIndices)) sampleIndices=1:ncol(data)
+	if(is.null(geneIndices)) geneIndices=1:nrow(data)
+	
+	temp <- data[geneIndices, sampleIndices]
+	if(model==1)
+	{
+		tempFit <- try(rasch(temp, constraint = cbind(ncol(temp) + 1, 1), ...), silent=TRUE)
+	}
+	if(model==2)
+	{# force all dscrmn the same. dscmn is contained as a pure vector in the coef.
+		if(guessing==FALSE) # no guessing
+		{
+			tempFit <- try(rasch(temp, ...), silent=TRUE)
+		} else # with guessing
+		{
+			tempFit <- try(tpm(temp, type = "rasch", ...) , silent=TRUE)
+		}
+	}
+	if(model==3)
+	{
+		if(guessing==FALSE) # no guessing
+		{
+			tempFit <- try(ltm(temp ~ z1, ...), silent=TRUE)
+		} else # with guessing
+		{
+			tempFit <- try(tpm(temp, ...) , silent=TRUE)
+		} 	
+	}
+	
+	return(list(fit=tempFit, model=model, guessing=guessing, sampleIndices=sampleIndices, geneIndices=geneIndices))
+}
+
---
+++ branches/integIRTy-ptong/integIRTy/R/intIRTeasyRun.R
@@ -0,0 +1,76 @@
+intIRTeasyRun <- 
+	function(platforms, model=3, guessing=FALSE, addPermutedScore=FALSE, fold=1, echo=TRUE, parallel=FALSE){
+	nPlat <- length(platforms)
+	########################################
+	# fit on individual platform
+	########################################
+	if(echo==TRUE){
+			cat('Performing ltm fit for each platform', '\n')	
+	}	
+	#fits <- list('vector')
+	fitFunc <- function(i) fitOnSinglePlat(data=platforms[[i]], model=model, guessing=guessing)	
+	## fits: a list of list
+	if(parallel==TRUE){
+		fits <- foreach(plat = 1:nPlat, .packages='integIRTy') %dopar% fitFunc(plat)
+	} else {
+		fits <- foreach(plat = 1:nPlat) %do% fitFunc(plat)
+	}	
+	########################################
+	# calculate latent trait estimates from each platform
+	########################################
+	if(echo==TRUE){
+			cat('Performing latent trait estimation for each platform', '\n')	
+	}
+	# assemble the item parameters and input matrix as a list: each individual platform followed by integrated data
+	a_list <- foreach(i=1:nPlat) %do% coef(fits[[i]]$fit)[, 'Dscrmn']; a_list[[length(a_list)+1]] <- c(unlist(a_list)) # append integrated parameter
+	b_list <- foreach(i=1:nPlat) %do% coef(fits[[i]]$fit)[, 'Dffclt']; b_list[[length(b_list)+1]] <- c(unlist(b_list)) # append integrated parameter
+	tempf <- function(i) {
+		if(guessing==TRUE) {
+			coef(fits[[i]]$fit)[, 'Gussng']
+		} else {
+			rep(0, ncol(platforms[[i]]))
+		}
+	}	
+	c_list <- foreach(i=1:nPlat) %do% tempf(i); c_list[[length(c_list)+1]] <- c(unlist(c_list)) # append integrated parameter
+	platform_list <- foreach(i=1:nPlat) %do% platforms[[i]]; 
+	platform_list[[length(platform_list)+1]] <- foreach(i=1:nPlat, .combine='cbind') %do% platforms[[i]]
+	compScoreFunc <- function(i){
+		computeAbility(platform_list[[i]], 
+						dscrmn=a_list[[i]],
+						dffclt=b_list[[i]], 
+						c=c_list[[i]])
+	}
+	# this is a matrix each column corresponding to a platform in the original order
+	if(parallel==TRUE){
+		estimatedScoreMat <- foreach(plat = 1:length(platform_list), .combine='cbind', .packages='integIRTy') %dopar% compScoreFunc(plat)
+	} else {
+		estimatedScoreMat <- foreach(plat = 1:length(platform_list), .combine='cbind') %do% compScoreFunc(plat)
+	}
+	
+	########################################
+	# if necessary, add permuted score for each platform as well as integrated data
+	########################################
+	if(addPermutedScore==TRUE){
+		if(echo==TRUE){
+			cat('Calculating permuted latent trait for each platform', '\n')	
+		}	
+		compPermutedScoreFunc <- function(i) {
+			calculatePermutedScoreByGeneSampling(platform_list[[i]], 
+						dscrmn=a_list[[i]],
+						dffclt=b_list[[i]], 
+						c=c_list[[i]], fold=fold)
+		}
+		if(parallel==TRUE){
+			permutedScoreMat <- foreach(plat = 1:length(platform_list), .combine='cbind', .packages='integIRTy') %dopar% compPermutedScoreFunc(plat)
+		} else {
+			permutedScoreMat <- foreach(plat = 1:length(platform_list), .combine='cbind') %do% compPermutedScoreFunc(plat)
+		}
+		
+	}
+	# returns integrated latent trait and ltm fit on individual platform					
+	if(addPermutedScore==TRUE){
+		return(list(fits=fits, estimatedScoreMat=estimatedScoreMat, permutedScoreMat=permutedScoreMat, dscrmnList=a_list, dffcltList=b_list, gussngList=c_list))
+	} else {
+		return(list(fits=fits, estimatedScoreMat=estimatedScoreMat, dscrmnList=a_list, dffcltList=b_list, gussngList=c_list))
+	}
+}
